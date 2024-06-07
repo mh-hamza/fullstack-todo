4153c29185493d9ed5e37ece8a91b77b095caa75
Mohammad Hamza <mhdkh444@gmail.com>
2024-06-07 19:56:37 +0530

    make new buttons

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,13 @@ function App() {
     <div>
       <h1>TODO APP</h1>
       <p>made by mohammad hamza</p>
+      <form action="">
+        <input type="text" />
+        <button type="submit">Add</button>
+        <button type="submit">Delete</button>
+        <button type="submit">Edit</button>
+        
+      </form>
     </div>
   )
 }
