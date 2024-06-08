0613ebf5a8936c9b4fa7ca0a0a813fc22127653c
Mohammad Hamza <mhdkh444@gmail.com>
2024-06-08 12:50:22 +0530

    setup firebase config

--- src/context/Firebase.jsx
+++ src/context/Firebase.jsx
@@ -1,10 +1,19 @@
 import { createContext, useContext } from "react";
-
+import { initializeApp } from "firebase/app";
 
 const FirebaseContext = createContext(null);
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAs2wxfd9vSqKAWu9l4ZXYOtc8Y4bFJLYQ",
+  authDomain: "fullstack-todo-ee628.firebaseapp.com",
+  projectId: "fullstack-todo-ee628",
+  storageBucket: "fullstack-todo-ee628.appspot.com",
+  messagingSenderId: "800832624470",
+  appId: "1:800832624470:web:ac31d08de1656d760ee8f7"
+};
 
 export const useFirebase =()=> useContext(FirebaseContext)
+const firebaseApp = initializeApp(firebaseConfig)
 
 export const FirebaseProvider = (props) => {
   return (
