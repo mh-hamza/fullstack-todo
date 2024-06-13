8eec12050ff9a6efbfdd0e412a143f7b06e73027
Mohammad Hamza <mhdkh444@gmail.com>
2024-06-13 17:26:18 +0530

    start add to do

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,36 @@
 import React, { useEffect, useState } from "react";
 import { useFirebase } from "../context/Firebase";
+import AddTodo from "../components/AddTodo";
 
 const Home = () => {
   const [user, setUser] = useState("");
   const firebase = useFirebase();
   useEffect(() => {
     const fetchData = async () => {
-      await firebase.fetchUserData(); 
+      await firebase.fetchUserData();
       setUser(firebase.activeUserDetails);
     };
-  
-    fetchData(); 
+
+    fetchData();
     // console.log(user)
   }, [firebase]);
   return (
     <div>
-      {
-        user ? (
+      {user ? (
+        <>
           <div>
             <h1>Welcome: {user.name}</h1>
             <p>Email: {user.email}</p>
           </div>
-        ) : (
           <div>
-            <h1>Loading...</h1>
+            <AddTodo/>
           </div>
-        )
-      }
+        </>
+      ) : (
+        <div>
+          <h1>Loading...</h1>
+        </div>
+      )}
     </div>
   );
 };
